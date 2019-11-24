ecc8c047026822de1d0bf006a46b2520f143cb53
yuta kimura <outstandinglife718@gmail.com>
2019-11-24 13:05:26 +0900

    created data by faker

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('users')->truncate();
+
+        DB::table('users')->insert([
+            [
+                'name' => 'test',
+                'email' => 'test@test.com',
+                'password' => bcrypt('secret')
+            ],
+            [
+                'name' => 'test2',
+                'email' => 'test2@test.com',
+                'password' => bcrypt('secret')
+            ],
+            [
+                'name' => 'test3',
+                'email' => 'test3@test.com',
+                'password' => bcrypt('secret')
+            ],
+        ]);
+    }
+}
