88ce032415431ebf0c9ba05c76aad021ee8a159e
yuta kimura <outstandinglife718@gmail.com>
2020-06-03 15:49:06 +0900

    modified login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,7 +50,6 @@
 
         <br>
         <a href="{{ route('password.request') }}">パスワードをお忘れですか?</a><br>
-        <a href="{{ url('login/facebook')}}">Facebookでログイン</a>
 
     </div>
     <!-- /.login-box-body -->
